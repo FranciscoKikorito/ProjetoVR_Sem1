8a11986a8fb01563b9d283c1b3727271f9abb4b0
Francisco_Vilaca <francisco07032003@gmail.com>
2026-01-03 19:46:37 +0000

    nova scene e update scripts

--- Assets/Francisco/HandPunch.cs
+++ Assets/Francisco/HandPunch.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class HandPunch : MonoBehaviour
 {
     public int damage = 50;
+    public LayerMask enemyLayer;
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (((1 << collision.gameObject.layer) & enemyLayer) == 0)
+            return;
+
         Debug.Log("Apply damage via collision");
 
-        // Only hit objects with Health component
         Health health = collision.gameObject.GetComponent<Health>();
         if (health != null)
         {
