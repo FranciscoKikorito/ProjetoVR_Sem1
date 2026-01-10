d60a582b8f198dfef57a68f00e9feb114627b6b9
Francisco_Vilaca <francisco07032003@gmail.com>
2026-01-10 13:16:10 +0000

    update

--- Assets/Francisco/StatsUI.cs
+++ Assets/Francisco/StatsUI.cs
@@ -16,10 +16,10 @@ public class StatsDisplay : MonoBehaviour
 
         textComponent.text =
             $"<color=green>Health: {stats.health:0}</color>\n" +
+            $"<color=blue>Armor: {stats.armor:0}</color>\n" +
             $"<color=orange>Attack: {stats.attackDamage:0}</color>\n" +
             $"<color=purple>Alcohol Amplification: {stats.alcoholAmplification:0.00}</color>\n" +
             $"<color=red>Crit Chance: {critChance:0}%</color>\n" +
-            $"<color=yellow>Crit Damage: {stats.critDamage:0.00}x</color>\n" +
-            $"<color=blue>Armor: {stats.armor:0}</color>\n";
+            $"<color=yellow>Crit Damage: {stats.critDamage:0.00}x</color>\n";
     }
 }
