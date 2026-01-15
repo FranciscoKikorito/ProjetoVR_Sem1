389cf03ff9729bd5d096d923f272bfa361d6368d
jvpts11 <jvptsyt11@gmail.com>
2026-01-15 19:00:54 +0000

    ?

--- Assets/Jota/AugmentManager.cs
+++ Assets/Jota/AugmentManager.cs
@@ -589,7 +589,7 @@ public class AugmentManager : MonoBehaviour
 
         TextMeshPro textMesh = textObj.AddComponent<TextMeshPro>();
         textMesh.text = augment.augmentName + "\n" + augment.description;
-        textMesh.fontSize = 2f;
+        textMesh.fontSize = 1f;
         textMesh.alignment = TextAlignmentOptions.Center;
         textMesh.color = GetColorByRarity(augment.rarity);
         textMesh.textWrappingMode = TextWrappingModes.Normal;
